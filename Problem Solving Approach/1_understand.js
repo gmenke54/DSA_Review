b303d6fa9244cb7ee98af4234c6880095b132f12
gmenke54 <menke.gw@gmail.com>
2022-03-17 13:50:57 -0700

    began problem solving approach section

---
+++ Problem Solving Approach/1_understand.js
@@ -0,0 +1,18 @@
+// Step 1: Understand the Problem
+
+// Slow down / take a step back
+// Ask some clarifying questions:
+// Can you restate the problem in your own words?
+// What are the inputs and corresponding outputs?
+// Can the outputs be determiened from the inputs?
+// How should I labe the important parts of tthe problem?
+
+// EXAMPLE: Write a function that takes 2 numbers and returns their sum
+
+// Can you restate the problem in your own words?
+// implement addition
+// What are the inputs and corresponding outputs?
+// numbers; but are they small or large?; int? float? string?
+// Can the outputs be determiened from the inputs?
+
+// How should I labe the important parts of tthe problem?
---
+++ Problem Solving Approach/intro.js
@@ -0,0 +1,14 @@
+// What is an algorithm?
+//--> a process or set of steps to accomplish a certain task
+//--> common coding interview questions
+
+// How to get better:
+// 1. Devise a plan for solving problems
+// 2. Master common problem patterns
+
+// Approach:
+// Understand
+// Explore Example
+// Break it down
+// Solve
+// Refactor
