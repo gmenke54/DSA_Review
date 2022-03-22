f9f209aa7a3a436c939816377eba7b39f4afba3b
gmenke54 <menke.gw@gmail.com>
2022-03-22 15:43:42 -0700

    completed break it down notes

---
+++ Problem Solving Approach/2_examples.js
@@ -0,0 +1,15 @@
+// Explore Concrete Examples
+
+// examples can help you understand the problem better
+
+// start with simple examples
+// write these down w/ an input and an output
+
+// explore edge cases
+// empty input
+// big numbers, small numbers
+
+charCount('aaaa'); // {a:4}
+charCount('hello'); // {}
+
+// explore invalid inputs
---
+++ Problem Solving Approach/3_breakdown.js
@@ -0,0 +1,13 @@
+// Break it Down
+
+// explicitly write out the steps you must take
+// talk through each step as you  write it out
+
+function charCount(str) {
+  // create an object
+  // loop over the string
+  // if key exists and char is a num/letter, increment count
+  // if key doesnt exist and char is a num/letter, add key and set count to 1
+  // else, dont do anything
+  // return the object
+}
