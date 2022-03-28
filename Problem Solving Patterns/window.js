27e7ebda77254f0e30d28bdea29159dbdc388745
gmenke54 <menke.gw@gmail.com>
2022-03-28 12:49:48 -0700

    updated sliding window approach

--- Problem Solving Patterns/window.js
+++ Problem Solving Patterns/window.js
@@ -6,42 +6,31 @@
 //     for (let j = i; j < i + 4; j++) {
 //       currentSum += arr[j];
 //     }
+//     console.log('current sum:', currentSum);
 //     if (currentSum > maxSum) {
 //       maxSum = currentSum;
+//       console.log('set max sum:', maxSum);
 //     }
 //   }
 //   return maxSum;
 // };
 
 // sliding window:
-// const maxSub = (arr) => {
-//   let maxSum = arr[0] + arr[1] + arr[2] + arr[3];
-//   console.log('firstWindow:', maxSum);
-//   let currentSum = maxSum;
-//   for (let i = 1; i < arr.length - 3; i++) {
-//     console.log('NEW WINDOW');
-//     console.log([arr[i], arr[i + 1], arr[i + 2], arr[i + 3]]);
-//     currentSum -= arr[i - 1];
-//     currentSum += arr[i + 3];
-//     console.log(currentSum);
-//     if (currentSum > maxSum) {
-//       maxSum = currentSum;
-//     }
-//   }
-//   return maxSum;
-// };
-
-console.log(maxSub([5, 2, 4, 3, -1, 9, 3, 7, 6]));
-
 const maxSub = (arr) => {
-  let currentSum = arr[0] + arr[1] + arr[2] + arr[3];
-  let maxSum = currentSum;
+  let maxSum = arr[0] + arr[1] + arr[2] + arr[3];
+  console.log('firstWindow:', maxSum);
+  let currentSum = maxSum;
   for (let i = 1; i < arr.length - 3; i++) {
+    console.log('NEW WINDOW');
+    console.log([arr[i], arr[i + 1], arr[i + 2], arr[i + 3]]);
     currentSum -= arr[i - 1];
     currentSum += arr[i + 3];
+    console.log(currentSum);
     if (currentSum > maxSum) {
       maxSum = currentSum;
     }
   }
   return maxSum;
 };
+
+console.log(maxSub([5, 2, 4, 3, -1, 9, 3, 7, 6]));
