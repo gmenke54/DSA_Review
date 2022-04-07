569685269e75685adf84dd59a3136e852ed6e50b
gmenke54 <menke.gw@gmail.com>
2022-04-07 09:57:06 -0700

    completed all recursion logic

--- Recursion/challenges2.js
+++ Recursion/challenges2.js
@@ -61,6 +61,110 @@ const flatten = (array) => {
   return flat;
 };
 
-console.log(flatten([1, 2, 3, [4, [5, 6]]])); // [1, 2, 3, 4, 5]
+// console.log(flatten([1, 2, 3, [4, [5, 6]]])); // [1, 2, 3, 4, 5]
 
 // console.log(flatten([4, 5]));
+
+const capitalizeFirst = (arr) => {
+  let capitalized = [];
+  const helper = (remArr) => {
+    if (remArr.length <= 0) {
+      return;
+    }
+    capitalized.push(remArr[0].charAt(0).toUpperCase() + remArr[0].slice(1));
+    remArr.shift();
+    helper(remArr);
+  };
+  helper(arr);
+  return capitalized;
+};
+
+// console.log(capitalizeFirst(['car', 'taco', 'banana']));
+
+function nestedEvenSum(object) {
+  let sum = 0;
+  const cleanObject = (obj) => {
+    for (const key in obj) {
+      if (typeof obj[key] === 'object') {
+        cleanObject(obj[key]);
+      } else if (typeof obj[key] === 'number') {
+        console.log('its a number');
+        if (obj[key] % 2 === 0) {
+          sum += obj[key];
+        }
+      }
+    }
+  };
+  cleanObject(object);
+  return sum;
+}
+
+var obj2 = {
+  a: 2,
+  b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+  c: { c: { c: 2 }, cc: 'ball', ccc: 5 },
+  d: 1,
+  e: { e: { e: 2 }, ee: 'car' }
+};
+
+// console.log(nestedEvenSum(obj2)); // 10
+
+const stringifyNumbers = (object) => {
+  const cleanObject = (obj) => {
+    for (const key in obj) {
+      if (typeof obj[key] === 'object') {
+        cleanObject(obj[key]);
+      } else if (typeof obj[key] === 'number') {
+        obj[key] = obj[key].toString();
+      }
+    }
+  };
+  cleanObject(object);
+  return object;
+};
+
+let obj = {
+  num: 1,
+  test: [],
+  data: {
+    val: 4,
+    info: {
+      isRight: true,
+      random: 66
+    }
+  }
+};
+// console.log(stringifyNumbers(obj));
+
+const collectStrings = (object) => {
+  let arr = [];
+  const cleanObject = (obj) => {
+    for (const key in obj) {
+      if (typeof obj[key] === 'object') {
+        cleanObject(obj[key]);
+      } else if (typeof obj[key] === 'string') {
+        console.log('its a string');
+        arr.push(obj[key]);
+      }
+    }
+  };
+  cleanObject(object);
+  return arr;
+};
+const obj1 = {
+  stuff: 'foo',
+  data: {
+    val: {
+      thing: {
+        info: 'bar',
+        moreInfo: {
+          evenMoreInfo: {
+            weMadeIt: 'baz'
+          }
+        }
+      }
+    }
+  }
+};
+
+console.log(collectStrings(obj1)); // ["foo", "bar", "baz"])
