0950b5d327fb14a03c44c79e03edec1d196782ae
gmenke54 <menke.gw@gmail.com>
2022-04-12 13:34:22 -0700

    completed binary search logic

--- Searching Algorithms/binary.js
+++ Searching Algorithms/binary.js
@@ -18,9 +18,23 @@ const binarySearch = (arr, target) => {
     } else if (target < arr[middle]) {
       // target is smaller than current
       right = middle;
+      if (right === left) {
+        if (arr[left] === target) {
+          return left;
+        } else {
+          return -1;
+        }
+      }
     } else {
       // target is larger than current
       left = middle;
+      if (right - 1 === middle) {
+        if (arr[right] === target) {
+          return right;
+        } else {
+          return -1;
+        }
+      }
     }
   }
   return -1;
@@ -29,6 +43,6 @@ const binarySearch = (arr, target) => {
 console.log(
   binarySearch(
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 20, 21, 54, 56, 69],
-    56
+    1
   )
 );
