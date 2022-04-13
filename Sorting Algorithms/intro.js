8b388cc12d21607f7955f3f274bd367ec04dd065
gmenke54 <menke.gw@gmail.com>
2022-04-13 14:22:34 -0700

    completed sorting alg

---
+++ Sorting Algorithms/intro.js
@@ -0,0 +1,19 @@
+// Sorting Algorithms:
+// very important concept in computer science
+// the process of rearranging the items in a collection so that they are in some kind of order
+// lots of defined strategies of sorting algorithms
+
+// Why is this important?
+// incredibly common task
+// important to know which strategies work best when
+// different techniques have different advantages and disadvantages
+
+// Elementary:
+// Bubble
+// Selection
+// Insertion
+
+// Javascript has a built in sort method:
+// How does it work "under the hood?"
+// sorts lexographically (essentially alphabetically)
+// accepts a comparator function
