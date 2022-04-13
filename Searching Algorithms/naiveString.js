a5e808fff965030e7502b96de1574858b40ff294
gmenke54 <menke.gw@gmail.com>
2022-04-13 13:56:48 -0700

    completed logic for naive string search

---
+++ Searching Algorithms/naiveString.js
@@ -0,0 +1,28 @@
+// Naive String Search:
+
+// searching for a the frquency of a substring in a larger string
+
+const searchForSub = (string, target) => {
+  let counter = 0;
+  let targetPointer = 0;
+
+  for (let stringPointer = 0; stringPointer < string.length; stringPointer++) {
+    console.log('/////////////////////////');
+    console.log('string:', string[stringPointer]);
+    console.log('target:', target[targetPointer]);
+    if (string[stringPointer] === target[targetPointer]) {
+      targetPointer++;
+      console.log(targetPointer);
+      console.log(target.length);
+      if (target.length === targetPointer) {
+        counter++;
+        targetPointer = 0;
+      }
+    } else {
+      targetPointer = 0;
+    }
+  }
+  return counter;
+};
+
+console.log(searchForSub('ozmgnnnnomzgnnnnwomng', 'omg'));
