77c76eab6814e54b4917027355929433ce4fb293
gmenke54 <menke.gw@gmail.com>
2022-04-18 14:34:04 -0700

    initialized bubble sort

---
+++ Sorting Algorithms/bubble.js
@@ -0,0 +1,9 @@
+// Not typically used in code but is important to understand the concept
+
+// larger values will "bubble to the top"
+
+// Process:
+// Iterate through data set, comparing neighbors and swapping if need be based on sort criteria
+// repeat this until array is entirely sorted
+
+const bubbleSort = 
